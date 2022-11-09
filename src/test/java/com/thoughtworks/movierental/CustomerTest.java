49e360f5bfaf4873fa8d2452d6d571f0c7bf0cdf
Mazen Imad Jaber <mazen.jaber@thoughtworks.com>
2022-11-09 16:22:53 +1000

    add initial tests

--- src/test/java/com/thoughtworks/movierental/CustomerTest.java
+++ src/test/java/com/thoughtworks/movierental/CustomerTest.java
@@ -5,11 +5,30 @@ import org.junit.Test;
 
 public class CustomerTest {
     @Test
-    public void test() {
+    public void noMoviesTest() {
 
-        int expected = 3;
+        Customer testCustomer = new Customer("Sam");
+        System.out.println(testCustomer.statement());
+        String expected = "Rental Record for Sam\n" +
+                "Amount owed is 0.0\n" +
+                "You earned 0 frequent renter points";
 
-        Assert.assertEquals(expected, 1 + 2);
+        Assert.assertEquals(expected, testCustomer.statement());
+    }
+    @Test
+    public void oneMovieTest() {
+        Movie testMovie = new Movie("Avatar", 1);
+        Rental testRental = new Rental(testMovie,2);
+        Customer testCustomer = new Customer("Sam");
+        testCustomer.addRental(testRental);
+        System.out.println(testCustomer.statement());
+        String expected = "Rental Record for Sam\n" +
+                "\tAvatar\t6.0\n" +
+                "Amount owed is 6.0\n" +
+                "You earned 2 frequent renter points";
+
+
+        Assert.assertEquals(expected, testCustomer.statement());
     }
 
 }
