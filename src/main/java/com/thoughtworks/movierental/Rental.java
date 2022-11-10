da18bb0dabe95e192edf7d64133d9053dab86dc9
Mazen Imad Jaber <mazen.jaber@thoughtworks.com>
2022-11-10 15:50:57 +1000

    extract switch statement to function in the rental class

--- src/main/java/com/thoughtworks/movierental/Rental.java
+++ src/main/java/com/thoughtworks/movierental/Rental.java
@@ -17,4 +17,23 @@ public class Rental {
     public Movie getMovie() {
         return movie;
     }
+    public double getCostAmount() {
+        double amount = 0;
+        switch (movie.getPriceCode()) {
+            case Movie.REGULAR:
+                amount += 2;
+                if (daysRented > 2)
+                    amount += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                amount += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                amount += 1.5;
+                if (daysRented > 3)
+                    amount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return amount;
+    }
 }
--- src/test/java/com/thoughtworks/movierental/CustomerTest.java
+++ src/test/java/com/thoughtworks/movierental/CustomerTest.java
@@ -8,12 +8,12 @@ public class CustomerTest {
     public void noMoviesTest() {
 
         Customer testCustomer = new Customer("Sam");
-        System.out.println(testCustomer.statement());
+        System.out.println(Customer.statement(testCustomer));
         String expected = "Rental Record for Sam\n" +
                 "Amount owed is 0.0\n" +
                 "You earned 0 frequent renter points";
 
-        Assert.assertEquals(expected, testCustomer.statement());
+        Assert.assertEquals(expected, Customer.statement(testCustomer));
     }
     @Test
     public void oneMovieTest() {
@@ -21,14 +21,30 @@ public class CustomerTest {
         Rental testRental = new Rental(testMovie,2);
         Customer testCustomer = new Customer("Sam");
         testCustomer.addRental(testRental);
-        System.out.println(testCustomer.statement());
+        System.out.println(Customer.statement(testCustomer));
         String expected = "Rental Record for Sam\n" +
                 "\tAvatar\t6.0\n" +
                 "Amount owed is 6.0\n" +
                 "You earned 2 frequent renter points";
 
 
-        Assert.assertEquals(expected, testCustomer.statement());
+        Assert.assertEquals(expected, Customer.statement(testCustomer));
     }
 
+    @Test
+    public void shouldGenerateStatementForCustomer() {
+        Customer customer = new Customer("Bruce Wayne");
+        customer.addRental(new Rental(new Movie("Spider-man: No way home", Movie.NEW_RELEASE), 4));
+        customer.addRental(new Rental(new Movie("Spider-man: Homecoming", Movie.REGULAR), 4));
+        customer.addRental(new Rental(new Movie("Spider-man: Animated", Movie.CHILDRENS), 4));
+
+        String statement = Customer.statement(customer);
+
+        Assert.assertEquals("Rental Record for Bruce Wayne\n" +
+                "\tSpider-man: No way home\t12.0\n" +
+                "\tSpider-man: Homecoming\t5.0\n" +
+                "\tSpider-man: Animated\t3.0\n" +
+                "Amount owed is 20.0\n" +
+                "You earned 4 frequent renter points", statement);
+    }
 }
